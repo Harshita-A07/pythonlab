4a83a3ba87b4d1d12a17a379881e26a312c9fea3
Harshita-A07 <121336740+Harshita-A07@users.noreply.github.com>
2022-12-29 23:51:02 +0530

    Add files via upload

---
+++ exp03islower.py
@@ -0,0 +1,7 @@
+c=input('enter any character')
+if(c.isupper()==True):
+    print(c,"is in uppercse")
+elif(c,islower()==True):
+    print(c,"is in lowercase")
+else:
+    print("it is not a character")
